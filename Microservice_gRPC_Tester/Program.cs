667b22bd45a407125d2b2c63814bddd8aacedf2b
Thimoty Barbieri <thimoty@thimoty.it>
2022-02-09 19:33:37 +0100

    completed client

--- Microservice_gRPC_Tester/Program.cs
+++ Microservice_gRPC_Tester/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using Microservice_gRPC_Tester;
+
+var channel = GrpcChannel.ForAddress(new Uri("https://localhost:7035"));
+var client = new DistanceInfo.DistanceInfoClient(channel);
+ var response = await
+client.GetDistanceAsync(new Cities
+{ OriginCity = "Topeka,KS", DestinationCity = "Los Angeles,CA" });
+Console.WriteLine(response.Miles);
+Console.ReadKey();
+
+
